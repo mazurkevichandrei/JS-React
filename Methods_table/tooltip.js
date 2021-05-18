9fc72472607eb880d603d2213d84a376fd8b0b41
mazurkevichandrei <mazurkevi4@gmail.com>
2021-05-18 20:00:40 +0300

    add tooltip window

--- Methods_table/tooltip.js
+++ Methods_table/tooltip.js
@@ -33,3 +33,25 @@ if (methodsElement) {
         }
     })
 }
+
+const windowElement = document.querySelector('body')
+const toolTipElement = document.createElement('div')
+toolTipElement.classList.add('tool-tip')
+windowElement.append(toolTipElement)
+const itemsElements = document.querySelectorAll('li')
+
+const showToolTip = function(evt){
+    toolTipElement.textContent = `${evt.target.textContent} is ${evt.target.dataset.type} method`
+    toolTipElement.style.visibility = 'visible'
+}
+const hideToolTip = function(){
+    toolTipElement.style.visibility='hidden'
+}
+const positionToolTip = function(evt){
+    toolTipElement.style.visibility = 'visible'
+    toolTipElement.style.top = evt.clientY + 'px'
+    toolTipElement.style.left = evt.clientX + 15 + 'px'
+}
+itemsElements.forEach(item => item.addEventListener('mouseenter',showToolTip))
+itemsElements.forEach(item => item.addEventListener('mouseleave', hideToolTip))
+itemsElements.forEach(item => item.addEventListener('mousemove', positionToolTip))
