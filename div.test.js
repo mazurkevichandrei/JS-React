bac3c1d21843435bab9a5fdde7b4dae48dd658a3
mazurkevichandrei <mazurkevi4@gmail.com>
2021-05-23 15:23:13 +0300

    set up Webpack

--- div.test.js
+++ div.test.js
@@ -1,4 +1,4 @@
-const div = require('./div');
+const div = require('div');
 
 test('div 16 / 4 to equal 4', () => {
   expect(div(16, 4)).toBe(4);
--- multi.test.js
+++ multi.test.js
@@ -1,4 +1,4 @@
-const multi = require('./multi');
+const multi = require('multi');
 
 test('multi 10 * 3 to equal 30', () => {
   expect(multi(10, 3)).toBe(30);
