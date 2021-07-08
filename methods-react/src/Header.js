58f973a40d07846da81bfe08aea4035b1c3d0086
mazurkevichandrei <mazurkevi4@gmail.com>
2021-07-08 21:50:54 +0300

    'add_restart_game_button'

--- methods-react/src/Header.js
+++ methods-react/src/Header.js
@@ -11,12 +11,26 @@ import SlyledLink from './styleditems/styledLink'
 
 import { useSelector } from 'react-redux';
 import store from './store/store';
-import {resetCounter} from './store/reducers/test'
+import {counter, resetCounter} from './store/reducers/test'
 import StyledButton from './styleditems/styledButton'
+import { resetTypes } from './store/methods';
 
 const Header = ({data}) => {
     const count = useSelector((state) => state.counter);
+    const newList = useSelector((state) => state.methods);
     const total = count.steps===0 ? 0 : Math.round(count.correct/count.steps*100)
+
+    const restart = () => {
+        store.dispatch(resetCounter())
+        store.dispatch(resetTypes())
+    }
+    const restartButton = () =>{ if (count.steps>0){
+        return (
+            <button onClick={restart}>Try again</button>
+        )
+    }
+}
+
     return(
         <div style={style.header}>
             <h1>{data}</h1>
@@ -29,6 +43,8 @@ const Header = ({data}) => {
             <span>Errors: {count.errors} </span>
             </span>
             <span>Total: {total} %</span>
+            {restartButton()}
+            
             <StyledNavi>
                 <StyledNaviItem><SlyledLink to='/'>Home</SlyledLink></StyledNaviItem>
                 <StyledNaviItem><SlyledLink to='/methods'>Methods</SlyledLink></StyledNaviItem>
--- methods-react/src/store/methods.js
+++ methods-react/src/store/methods.js
@@ -6,16 +6,17 @@ export const methods = createSlice({
     name: 'methods',
     initialState:  {
         value: Object.getOwnPropertyNames(Array.prototype).map((item,index) =>({id:index+1,name:item,type:LIST_TYPES.MAIN})),
-        // filteredValue: '',
-        // filterValue: '',
     },
     reducers: {
         changeType: (state, {payload}) => {
             const {name, type} = payload
             changeMethodType(state.value, name , type)
         },
+        resetTypes: (state) => {
+            state.value.map(item=> item.type=LIST_TYPES.MAIN)
+        }
     }
 });
 
-export const { changeType, runFil } = methods.actions;
+export const { changeType, resetTypes } = methods.actions;
 export default methods.reducer;
