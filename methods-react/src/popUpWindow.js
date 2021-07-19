44b46430cd36eb21e77284b8f01a2c58f2e4b0ff
mazurkevichandrei <mazurkevi4@gmail.com>
2021-07-19 13:36:29 +0300

    'overflowY_added_instead_of_overflow'

--- methods-react/src/popUpWindow.js
+++ methods-react/src/popUpWindow.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext} from 'react';
 import { Context } from './context';
 import StyledPopUp from './styleditems/popUp/styledPopUp';
 import StyledRestart from './styleditems/styledRestart';
